2754788763c2139b4ad4852ce76287f10a47db5a
Gayathri <gayathriram2907@gmail.com>
2025-12-09 10:18:46 -0800

    updated for Week4

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,15 +1,20 @@
-{/*extract from TodoList.jsx*/}
-function TodoList(){
-    const todos =[
-    {id:1,title:"review resources"},
-    {id:2,title:"take notes"},
-    {id:3,title:"code out app"},
-  ]
-    return(
-        <ul>
-        {todos.map(todo=> <li key ={todo.id}>{todo.title}</li>)}
-      </ul>
-    )
+{/* extract from TodoList.jsx */}
+import TodoListItem from "./TodoListItem.jsx";
 
+function TodoList() {
+
+   const todos = [
+    {id: 1, title: "review resources"},
+    {id: 2, title: "take notes"},
+    {id: 3, title: "code out app"},
+ ]
+ 
+ return(
+    <>
+        <ul>
+            {todos.map(todo => <TodoListItem key={todo.id} todo={todo} />)}
+        </ul>
+    </>
+ )
 }
-export default TodoList
+export default TodoList;
